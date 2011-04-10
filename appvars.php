bd070dc0d18f02ee4d8b3e7333f6c6dc352ab7fe
philipbmw <nogood79dle@gmail.com>
2011-04-10 19:55:56 +0900

    philip

--- appvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define application constants
-  define('MM_UPLOADPATH', 'images/');
-  define('MM_MAXFILESIZE', 327680);      // 32 KB
-  define('MM_MAXIMGWIDTH', 1200);        // 120 pixels
-  define('MM_MAXIMGHEIGHT', 1200);       // 120 pixels
-?>
